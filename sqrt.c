fe37f4ec673588953dc9d8a77855b4533fa909e6
Драганчук Алена Сергеевна <ri57s05@awp10.csc.local>
2016-05-31 09:26:45 +0600

    Add d>0

--- sqrt.c
+++ sqrt.c
@@ -6,4 +6,11 @@ int main()
 	printf("Enter a, b, c: ");
 	scanf("%lf %lf %lf", &a, &b, &c);
 	d = b * b - 4 * a * c;
+	if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / 2 * a;
+		x2 = (-b - sqrt(d)) / 2 * a;
+		printf("x1=%.2lf.\n", x1);
+		printf("x2=%.2lf.\n", x2);
+	}
 }
